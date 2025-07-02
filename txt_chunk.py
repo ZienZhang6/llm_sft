3e088a64a0d3566dfade3a459ece3e57ef8840d8
enen <zhangzien1762062129@gmail.com>
2025-07-02 11:38:30 +0800

    fix：优化代码

--- txt_chunk.py
+++ txt_chunk.py
@@ -65,7 +65,26 @@ def clean_text(text_list: list) -> list:
     """
     return [line.replace('\u3000\u3000', '').replace('\n', '') for line in text_list]
 
+def split_text_into_chunks(cleaned_text_list: list, chapter_indices: list) -> list:
+    """
+    将文本按照章节分割为多个块，确保最后一个章节不会漏掉。
 
+    参数:
+    cleaned_text_list (list): 清理后的文本列表，每个元素为一段文本。
+    chapter_indices (list): 章节索引列表，每个元素为章节标题在cleaned_text_list中的索引。
+
+    返回:
+    list: 分割后的文本块列表。
+    """
+    chunk_list = []
+    for i in range(len(chapter_indices)):
+        if i != len(chapter_indices) - 1:
+            # 章节标题 + \n + 内容
+            chunk_list.append(''.join(cleaned_text_list[chapter_indices[i]]) + '\n' + ''.join(cleaned_text_list[chapter_indices[i] + 1:chapter_indices[i + 1]]))
+        else:
+            chunk_list.append(''.join(cleaned_text_list[chapter_indices[i]]) + '\n' + ''.join(cleaned_text_list[chapter_indices[i] + 1:]))
+    
+    return chunk_list
 
 if __name__ == '__main__':
 
@@ -79,9 +98,6 @@ if __name__ == '__main__':
     # 对列表中的每一项进行字符剔除操作
     cleaned_text_list = clean_text(long_text)
 
-    # # 合并列表项为一个字符串
-    # cleaned_text = ''.join(cleaned_text_list)
-
     # 获取章节索引 
     chapter_indices = get_chapter_indices(cleaned_text_list)
 
@@ -91,16 +107,7 @@ if __name__ == '__main__':
         print(f"第 {i+1} 章: {cleaned_text_list[chapter_indices[i]].strip()}")
 
     # 每章节存为一个chunk 确保最后一个章节不会漏掉
-    chunk_list = []
-    for i in range(len(chapter_indices)):
-        if i != len(chapter_indices) - 1:
-            # 章节标题  + /n + 内容
-            chunk_list.append(''.join(cleaned_text_list[chapter_indices[i]]) + '\n' + ''.join(cleaned_text_list[chapter_indices[i]+1:chapter_indices[i+1]]))
-        else:
-            chunk_list.append(''.join(cleaned_text_list[chapter_indices[i]]) + '\n' + ''.join(cleaned_text_list[chapter_indices[i]+1:-1]))
-
-    # 打印分割后的文本块
-    print("分割后的文本块:", chunk_list)
+    chunk_list = split_text_into_chunks(cleaned_text_list, chapter_indices)
 
     # 保存分割后的文本块到指定目录
     save_chunks_to_files(chunk_list, output_dir)
