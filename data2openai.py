568893cb472a20115032133dadf520757f401389
enen <zhangzien1762062129@gmail.com>
2025-07-31 10:27:10 +0800

    feat： 原始预料处理成openai的格式

---
+++ data2openai.py
@@ -0,0 +1,86 @@
+import os
+import pandas as pd
+
+
+# 读取目录中的所有 Parquet 文件
+def read_multiple_parquet_files(file_path):
+    # 获取目录下指定的 Parquet 文件
+    df = pd.read_parquet(file_path)
+    return df
+
+
+def save_to_jsonl(df, file_path):
+    df.to_json(file_path, orient='records', lines=True)
+    print(f"数据已保存为{file_path}")
+
+
+def build_dataset_format(row):
+    new_message = []
+    choices = ['A', 'B', 'C', 'D', 'E']
+    question = row['question'] + '\n'
+    for choice in choices:
+        if not row[choice]:
+            break
+        question = question + choice + ':' + row[choice] + '\n'
+    new_message.append({'role': 'user', 'content': question})
+    answer = row['explanation'] + '\nSo the correct answer is ' + row['answer'] + '\n'
+    new_message.append({'role': 'assistant', 'content': answer})
+    row['messages'] = new_message
+    row['meta_info'] = {
+        "domain": "instruction",
+        "source": "Telecom-QA-MultipleChoice/" + row["category"],
+        "task": "chat",
+        "ground_truth": row['answer']
+    }
+    return row
+
+
+def build_test_format(row):
+    choices = ['A', 'B', 'C', 'D', 'E']
+    question = row['question'] + '\n'
+    for choice in choices:
+        if not row[choice]:
+            break
+        # 修正缩进，将该行移到循环内部
+        question = question + choice + ': ' + row[choice] + '\n'
+    row['source'] = "Telecom-QA-MultipleChoice/" + row["category"]
+    row['question'] = question
+    return row  
+
+
+def jsonl_to_json(jsonl_file, json_file):
+    import json
+    with open(jsonl_file, 'r') as f:
+        data = [json.loads(line) for line in f]
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    file_path = 'E:/learning/llm_sft/Telecom-QA-MultipleChoice/data/train-00000-of-00001.parquet'
+    df = read_multiple_parquet_files(file_path)
+    
+    # 显示前几行数据
+    print("原始数据:")
+    print(df.head())
+    
+    # 应用函数到每一行
+    df = df.apply(build_dataset_format, axis=1)
+   
+    # 删除无用列
+    df.drop(
+        columns=[
+            'question_id', 'question', 'A', 'B', 'C', 'D', 'E',
+            'answer', 'explanation', 'category', 'tag'
+        ],
+        inplace=True
+    )
+    df["idx"] = df.index
+    df.insert(0, 'idx', df.pop('idx'))
+   
+    # 显示抽取的数据
+    print("抽取后的数据:")
+    print(df.head())
+    
+    # 将抽取的数据保存为新的JSONL文件
+    save_to_jsonl(df, './sft_data/TelecomQA/data/train.jsonl')
